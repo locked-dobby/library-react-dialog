47b4fc0421b53cb0ced8006fb13fb7ef95e85a35
hpark <dark1451@gmail.com>
2024-08-02 10:45:34 +0900

    fix: file name bug

---
+++ examples/react/src/app.tsx
@@ -0,0 +1,13 @@
+import "./app.css";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./component/router";
+
+function App() {
+    return (
+        <>
+            <RouterProvider router={router} />
+        </>
+    );
+}
+
+export default App;
